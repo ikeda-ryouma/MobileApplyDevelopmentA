2011b35621112fb943912ce88d9a83e2fd653251
Ryouma Ikeda <175948608+ikeda-ryouma@users.noreply.github.com>
2025-09-03 16:21:22 +0900

    データのやり取りができるようになった

--- app/src/main/java/jp/ac/meijou/android/s241205011/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205011/MainActivity2.java
@@ -5,10 +5,14 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Optional;
 
 import jp.ac.meijou.android.s241205011.databinding.ActivityMain2Binding;
 import jp.ac.meijou.android.s241205011.databinding.ActivityMainBinding;
@@ -33,11 +37,40 @@ public class MainActivity2 extends AppCompatActivity {
             var intent = new Intent(this, MainActivity3.class);
             startActivity(intent);
         });
+        binding.button3.setOnClickListener(v -> {
+            var text = binding.editTextText3.getText().toString();
+            var intent = new Intent(this, MainActivity4.class);
+            intent.putExtra("text", text);
+            startActivity(intent);
+        });
         binding.button2.setOnClickListener(v -> {
             var intent = new Intent();
             intent.setAction(Intent.ACTION_VIEW);
             intent.setData(Uri.parse("https://hololive.hololivepro.com/talents/amane-kanata/"));
             startActivity(intent);
         });
+        binding.button5.setOnClickListener(v -> {
+            var intent = new Intent(this, MainActivity4.class);
+            getActivityResult.launch(intent);
+        });
     }
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                switch (result.getResultCode()) {
+                    case RESULT_OK -> {
+                        Optional.ofNullable(result.getData())
+                                .map(data -> data.getStringExtra("ret"))
+                                .map(text -> "Result: " + text)
+                                .ifPresent(text -> binding.textView2.setText(text));
+                    }
+                    case RESULT_CANCELED -> {
+                        binding.textView2.setText("Result: Canceled");
+                    }
+                    default -> {
+                        binding.textView2.setText("Result: Unknown(" + result.getResultCode() + ")");
+                    }
+                }
+            }
+    );
 }
